3e6d25964e285d871f795dc30431e15a6cac04e5
wageme <blueyguy@live.com>
2023-06-28 21:45:39 -0400

    Randomize Hello World

--- untitled/src/main.rs
+++ untitled/src/main.rs
@@ -1,7 +1,23 @@
+use rand::Rng;
+
 fn main() {
 
-    let greeting: String = "Hello".to_string();
-    let subject: String = "World".to_string();
+    // Vectors of Greetings and Subjects
+    let greetings = vec!["Hello", "Hi", "Greetings", "Salutations", "Hola", "Salut", "Allo", "Hallo"];
+    let subjects = vec!["World", "Sir", "Madam", "Captain"];
+
+    /*
+    let greet_index = rand::thread_rng().gen_range(0..greetings.len());
+    let subject_index = rand::thread_rng().gen_range(0..subjects.len());
+*/
+
+    // Get random greeting and subject
+    let greeting = greetings[rand::thread_rng().gen_range(0..greetings.len())];
+    let subject =  subjects[rand::thread_rng().gen_range(0..subjects.len())];
 
     println!("{}, {}!", greeting, subject);
+
+    if greeting == "Hello" && subject == "World" {
+        println!("Working as intended :)")
+    }
 }
