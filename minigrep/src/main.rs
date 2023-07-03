a355b785469e48dbaf2ab8ebf1a0f33444b4e49e
wageme <blueyguy@live.com>
2023-07-03 15:43:46 -0400

    feat: write error to std error

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,14 +2,17 @@
 Type in "cargo run [args]" in the terminal
 
 To search as normal, write:
-cargo run the "C:\Users\Wage Me\Desktop\rust\GitRust\minigrep\src\poem.txt"
+cargo run the poem.txt
 
 To search while ignoring case, write:
-$Env:IGNORE_CASE=1; cargo run a "C:\Users\Wage Me\Desktop\rust\GitRust\minigrep\src\poem.txt"
+$Env:IGNORE_CASE=1; cargo run a poem.txt
 C:\Users\Wage Me\Desktop\rust\GitRust\minigrep
 
 To make ignore case persist in powershell, write:
 Remove-Item Env:IGNORE_CASE
+
+To redirect output, write:
+cargo run to poem.txt > output.txt
 */
 
 use std::env; // environment variables
@@ -19,20 +22,14 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //dbg!(args);
-
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}" , config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
-
 }
