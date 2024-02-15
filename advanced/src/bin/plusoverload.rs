c57ecb9b44852897506725a682a1d413ac3617e6
wageme <blueyguy@live.com>
2024-02-15 02:43:50 -0500

    working on advanced stuff

--- advanced/src/bin/plusoverload.rs
+++ advanced/src/bin/plusoverload.rs
@@ -52,5 +52,40 @@ fn main() {
         Point { x: 2, y: 0 }
     );
 
-    println!("All good!")
+    println!("All good!");
+
+
+    type Kilometers = i32;
+
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+
+    println!("x + y = {}", x+y);
+
+    type Thunk = (i32, String);
+    let f: Thunk = (2, "Bruh".to_string());
+
+    println!("{:?}", f);
+
+    println!("The answer is: {}", do_twice(add_one, 5));
+
+    let mut functions: Vec<fn(String) -> i32> = vec![say_something];
+    functions[0]("hmm".to_string());
+
+    
+
+}
+
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
+fn say_something(s: String) -> i32 {
+    println!("{s}");
+    1
 }
